26f1b24bd0413c4de99f5c7e607d29e378a6b59e
Marius Achim <mariusachim2004@gmail.com>
2026-04-13 20:25:38 +0100

    refactor: nav bar background color is no longer transparent

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -64,7 +64,7 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="bg-brand-bg border-b border-brand-border sticky top-0 z-50">
+    <nav className="bg-nav-bg border-b border-brand-border sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
