2337edb7942e81e879a38409ae034bb4fe9a9405
timy <148886321+iguanalog@users.noreply.github.com>
2026-04-14 18:34:13 +0100

    refactor: changed union types (#41)

--- src/types/ingredient.types.ts
+++ src/types/ingredient.types.ts
@@ -1,32 +1,36 @@
-export type IngredientType =
-  | "CEREALS_AND_DERIVATIVES"
-  | "TUBERS"
-  | "VEGETABLES"
-  | "FRUIT"
-  | "DAIRY_PRODUCTS"
-  | "MEAT"
-  | "FISH"
-  | "EGGS"
-  | "LEGUMES"
-  | "FATS_AND_OILS";
+export const ingredientTypeOptions = {
+  CEREALS_AND_DERIVATIVES: "Cereals and derivatives",
+  TUBERS: "Tubers",
+  VEGETABLES: "Vegetables",
+  FRUIT: "Fruit",
+  DAIRY_PRODUCTS: "Dairy products",
+  MEAT: "Meat",
+  FISH: "Fish",
+  EGGS: "Eggs",
+  LEGUMES: "Legumes",
+  FATS_AND_OILS: "Fats and oils",
+} as const;
+export type IngredientType = keyof typeof ingredientTypeOptions;
 
-export type IngredientAllergen =
-  | "NONE"
-  | "GLUTEN_CONTAINING_CEREALS"
-  | "NUTS"
-  | "CRUSTACEANS"
-  | "CELERY"
-  | "EGGS"
-  | "MUSTARD"
-  | "FISH"
-  | "SESAME_SEEDS"
-  | "PEANUTS"
-  | "SULPHITES"
-  | "SOYBEANS"
-  | "LUPINS"
-  | "MILK_AND_MILK_PRODUCTS"
-  | "MOLLUSCS";
-
+export const ingredientAllergenOptions = {
+  NONE: "None",
+  GLUTEN_CONTAINING_CEREALS: "Gluten-containing cereals",
+  NUTS: "Nuts",
+  CRUSTACEANS: "Crustaceans",
+  CELERY: "Celery",
+  EGGS: "Eggs",
+  MUSTARD: "Mustard",
+  FISH: "Fish",
+  SESAME_SEEDS: "Sesame seeds",
+  PEANUTS: "Peanuts",
+  SULPHITES: "Sulphites",
+  SOYBEANS: "Soybeans",
+  LUPINS: "Lupins",
+  MILK_AND_MILK_PRODUCTS: "Milk and milk products",
+  MOLLUSCS: "Molluscs",
+} as const;
+export type IngredientAllergen = keyof typeof ingredientAllergenOptions;
+ 
 export interface IngredientResponse {
   readonly id: string;
   /** * Ingredient name.
